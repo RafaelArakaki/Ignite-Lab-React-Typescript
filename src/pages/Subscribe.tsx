2d2fe6ba77cb36b6f43933c43ab75d17e47e090a
Rafael S Arakaki <rafael.arakaki@wct.local>
2022-06-26 15:30:56 -0300

    Ajuste

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,7 +2,7 @@ import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
-import { imgURL } from "../assets/img/mockup-code.png"
+import imgURL from "../assets/img/mockup-code.png"
 
 export function Subscribe(){
   const navigate = useNavigate();
