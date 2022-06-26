058a2f0804ed40443fc0acd18caf2db481464323
Rafael S Arakaki <rafael.arakaki@wct.local>
2022-06-26 13:29:34 -0300

    Desenvolvimento da página de cadastro

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o543o40mg001z7fdde2zsr/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
